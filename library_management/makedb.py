4076f7f7a72f9b9859ff68d830647d61560d4efc
Sahib Singh <85299873+sahib2565@users.noreply.github.com>
2022-05-02 19:45:43 +0200

    Updated the dates and the db structure

--- library_management/makedb.py
+++ library_management/makedb.py
@@ -6,11 +6,11 @@ c = conn.cursor()
 
 
 def main():
-    '''
     c.execute("""CREATE TABLE books(
                 book_id TEXT PRIMARY KEY,
-                author TEXT DEFAULT "UNKNOWN",
-                release TEXT DEFAULT "UNKNOWN",
+                author_name TEXT DEFAULT "UNKNOWN",
+                author_surname TEXT DEFAULT "UNKNOWN",
+                release date DEFAULT "UNKNOWN",
                 book_name TEXT NOT NULL,
                 availabilty TEXT not NULL
             )""")
@@ -18,15 +18,14 @@ def main():
                 id_book TEXT,
                 relase_date date NOT NULL
             )""")
-    '''
-    list_books = [('38907', 'JK Rowling', '26/06/1997', 'Harry Potter:1', 'YES'),
-                  ('39893', 'JK Rowling', '02/07/1998', 'Harry Potter:2', 'YES'),
-                  ('33375', 'JK Rowling', '08/07/1999', 'Harry Potter:3', 'YES'),
-                  ('10978', 'J.R.R Tolkien', '21/09/1937', 'The lord of the rings:1', 'YES'),
-                  ('10988', 'J.R.R Tolkien', '29/07/1954', 'The lord of the rings:2', 'YES'),
-                  ('87190', 'George Orwell', '08/06/1949', '1984', 'YES')
+    list_books = [('38907', 'JK', 'Rowling', '1997-06-26', 'Harry Potter:1', 'YES'),
+                  ('39893', 'JK', 'Rowling', '1998-07-02', 'Harry Potter:2', 'YES'),
+                  ('33375', 'JK', 'Rowling', '1999-07-08', 'Harry Potter:3', 'YES'),
+                  ('10978', 'J.R.R', 'Tolkien', '1937-09-21', 'The lord of the rings:1', 'YES'),
+                  ('10988', 'J.R.R', 'Tolkien', '1954-07-29', 'The lord of the rings:2', 'YES'),
+                  ('87190', 'George', 'Orwell', '1949-06-08', '1984', 'YES')
                   ]
-    # c.executemany("INSERT into books values(?,?,?,?,?)", list_books)
+    c.executemany("INSERT into books values(?,?,?,?,?,?)", list_books)
     conn.commit()
     conn.close()
 
